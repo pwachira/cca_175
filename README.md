f24039e0e8063b2b06a6bf73448c922645818676
peter_wachira <peterwchr@gmail.com>
2018-03-04 17:12:10 -0500

    add csv package

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ### What is this repository for? ###
 
 cca 175 certification tutorial as provided in http://arun-teaches-u-tech.blogspot.com/p/certification-preparation-plan.html
+
+To run write/read to/from  csv using spark shell, use the following package option:
+    spark-shell --packages com.databricks:spark-csv_2.10:1.5.0 
+
+
---
+++ problem1/3_df_to_parquet_csv.scala
@@ -0,0 +1,11 @@
+
+val oi = sqlContext.read.format("com.databricks.spark.avro").load("/user/cloudera/problem1/order_items")
+val o = sqlContext.read.format("com.databricks.spark.avro").load("/user/cloudera/problem1/orders").
+      withColumn("order_date_dt",from_unixtime($"order_date"/1000,"yyyy-MM-dd")).
+      drop("order_date").
+      withColumnRenamed("order_date_dt","order_date").
+      join(oi,$"order_id" === $"order_item_order_id").
+      groupBy("order_status","order_date").
+      agg(countDistinct("order_id").as("total_orders"),sum("order_item_subtotal").as("total_amount"))
+sqlContext.setConf("spark.sql.parquet.compression.codec","uncompressed")
+o.write.format("com.databricks.spark.csv").save("/user/cloudera/problem1/result4a-csv")
