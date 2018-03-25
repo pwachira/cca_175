fcd6ee63b17c93b98ae29e3ab6d5cbdb37279621
cloudera <cloudera@quickstart.cloudera>
2018-03-24 17:56:03 -0700

    sequence to orc

--- problem4/8_sequence_to_orc.scala
+++ problem4/8_sequence_to_orc.scala
@@ -1,2 +1,7 @@
-sc.sequenceFile("/user/cloudera/problem5/sequence",classOf[Int],classOf[String]).
-	
+import org.apache.hadoop.io.Text
+import org.apache.hadoop.io.IntWritable
+val oDF = sc.sequenceFile("/user/cloudera/problem5/sequence",classOf[IntWritable],classOf[Text]).
+	map(o => (o._1.get,o._2.toString.split("\t"))).
+	map(o => (o._1,o._2(0),o._2(1),o._2(2))).
+	toDF
+oDF.write.orc("/user/cloudera/problem5/orc")
