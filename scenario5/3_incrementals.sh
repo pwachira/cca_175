574662475617114fedc6d06b9e68b98f2decd4b7
cloudera <cloudera@quickstart.cloudera>
2018-03-28 21:13:05 -0700

    incremental to hive

--- scenario5/3_incrementals.sh
+++ scenario5/3_incrementals.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 hdfs dfs -rm -r /user/cloudera/problem5/products-incremental
+
 mysql -u "retail_dba" --password="cloudera"  --database="retail_db" \
 	-e "delete from products_replica where product_id > 1345;"
 
 sqoop job --delete incremental
-sqoop job --create incremental -- import --connect jdbc:mysql://localhost/retail_db --username retail_dba --password cloudera \
+sqoop job --create incremental -- import --direct --connect jdbc:mysql://localhost/retail_db --username retail_dba --password cloudera \
 	--as-textfile --table products_replica --target-dir /user/cloudera/problem5/products-incremental \
 	--check-column product_id --incremental append --last-value 0
+
+sqoop job --exec incremental
 
 mysql -u "retail_dba" --password="cloudera"  --database="retail_db" \
 	-e "insert into products_replica (product_id) values(1346); insert into products_replica (product_id) values(1347);insert into products_replica (product_id) values(1348);"
--- scenario5/4_incrementals.sh
+++ scenario5/4_incrementals.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 beeline -u  jdbc:hive2:// hive cloudera \
-	-e "drop table if exists products_hive";
+	-e "create database if not exists problem5;"
 
 beeline -u  jdbc:hive2:// hive cloudera \
-	-e "create table products_hive (product_id int, product_category_id int, product_name string, product_description string, product_price float, product_imaage string,product_grade int,  product_sentiment string);"
+	-e "create table if not exists problem5.products_hive (product_id int, product_category_id int, product_name string, product_description string, product_price float, product_image string,product_grade int,  product_sentiment string); truncate table problem5.products_hive"
+mysql -u "retail_dba" --password="cloudera"  --database="retail_db" \
+        -e "delete from products_replica where product_id > 1350;"
+
+sqoop job --delete incremental_2
+
+sqoop job --create incremental_2 -- import --direct --connect jdbc:mysql://localhost/retail_db --username retail_dba --password cloudera \
+        --table products_replica --hive-import --hive-database problem5 --hive-table products_hive \
+        --check-column product_id --incremental append --last-value 0
+sqoop job --exec incremental_2
+mysql -u "retail_dba" --password="cloudera"  --database="retail_db" \
+        -e "insert into products_replica (product_id) values(1351); insert into products_replica (product_id) values(1352);insert into products_replica (product_id) values(1353);"
+wait
+sqoop job --exec incremental_2
+mysql -u "retail_dba" --password="cloudera"  --database="retail_db" \
+        -e "delete from products_replica where product_id > 1353; insert into products_replica (product_id) values(1354); insert into products_replica (product_id) values(1355);"
+wait
+sqoop job --exec incremental_2
